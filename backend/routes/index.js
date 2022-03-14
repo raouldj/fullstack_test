92ab1a28ae18977a03d3c5c52e81915515165da6
Raul Kerstenbeck <raul.kerstenbeck@finanskompetens.se>
2022-03-14 23:35:17 +0100

    Empty frontend & backend

---
+++ backend/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/articles', function(req, res, next) {
+  res.json([{id: 1}])
+});
+
+module.exports = router;
